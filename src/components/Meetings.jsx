5f46f2a2469ab58916a35b678ad2497e963515c2
Ujjwal-here <ujjwalsbx@gmail.com>
2023-10-06 14:33:36 +0530

    Meetings Page Added

---
+++ src/components/Meetings.jsx
@@ -0,0 +1,12 @@
+import {ReusableCardOne} from "../reusable/ReusableCardOne";
+import "./Meetings.css"
+
+export function Meetings() {
+    return (
+        <div className="cards">
+            <ReusableCardOne/>
+            <ReusableCardOne/>
+        </div>
+
+    )
+}
