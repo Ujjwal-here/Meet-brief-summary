a96de951b5002c7799f6d48f7b34f3306242b502
Ujjwal-here <ujjwalsbx@gmail.com>
2023-10-09 15:38:33 +0530

    Routing Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Home} from "./components/Home";
+import React from "react";
 import Navbar from "./components/Navbar";
+import {Footer} from "./components/Footer";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home/>
+    },
+    {
+        path: "/login",
+        element: <h1>Login</h1>
+    },
+    // {
+    //     path: "/meetings",
+    //     element: <Meetings/>
+    // }
+])
 
 function App() {
-    return (
-        <div className="App">
-            <Navbar/>
-        </div>
-    );
+    return <>
+        <Navbar/>
+        <RouterProvider router={router}/>
+        <Footer/>
+    </>;
 }
 
 export default App;
