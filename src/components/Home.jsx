1c32815d953c3bd862a1f256f11998630c55d0aa
Ujjwal-here <ujjwalsbx@gmail.com>
2023-10-09 15:37:19 +0530

    Layout Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,17 @@
 import './Home.css'
-import MeetingPic from '../undraw_remote_meeting_re_abe7.svg'
 import {KeyFeatures} from "./KeyFeatures";
 import {HowExtensionWorks} from "./HowExtensionWorks";
+import {Introduction} from "./Introduction";
 
 export function Home() {
     return (
         <>
+            {/*-----Introduction-----*/}
             <section className="home-s1-main">
-                <div className="home-s1-main-div">
-                    <div className="home-s1-main-div-div">
-                        <p></p>
-                        <h1 className="home-s1-main-div-div-h1">Revolutionize Your Google Meet Experience with Real-time
-                            Captions</h1>
-                        <p className="home-s1-main-div-div-p">Tired of struggling to keep up with meeting notes? Our
-                            Chrome
-                            extension revolutionizes the way you capture and summarize Google Meet conversations. Say
-                            goodbye to manual note-taking and hello to instant, insightful summaries!</p>
-                        <button className="home-s1-main-div-div-btn">Download Now</button>
-                    </div>
-                    <img className="home-s1-main-div-img" src={MeetingPic} alt="Remote Meeting"/>
-                </div>
+                <Introduction/>
             </section>
 
-
+            {/*-----KeyFeatures-----*/}
             <section className="home-s2-main">
                 <div className="container">
                     <h2 className="home-s2-main-h2">Key Features</h2>
@@ -30,7 +19,7 @@ export function Home() {
                 </div>
             </section>
 
-
+            {/*-----HowExtensionWorks-----*/}
             <section className="home-s3-main">
                 <div className="container">
                     <h2 className="home-s3-main-h2">How Extension Works</h2>
