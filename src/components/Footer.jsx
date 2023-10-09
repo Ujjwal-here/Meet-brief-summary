c386b24dbd8330ee66034bcf9f73f3a352ba2372
Ujjwal-here <ujjwalsbx@gmail.com>
2023-10-09 14:18:40 +0530

    Footer Added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+export function Footer() {
+    return (
+        <footer className="footer-main">
+            <div className="footer-main-container">
+                <p className="footer-main-h1">For any inquiries or assistance, please feel free to reach out to us
+                    at:<span>support@meet-breif.com</span></p>
+                <p className="footer-main-p">© 2023 Meet-Brief. All Rights Reserved.</p>
+            </div>
+        </footer>
+    )
+}
