42d1e1a983954f97adee20f9cbe6cf04d0f0f0e1
Ujjwal-here <ujjwalsbx@gmail.com>
2023-10-09 14:58:38 +0530

    Change in Layout

--- src/reusable/KeyFeaturesCard.jsx
+++ src/reusable/KeyFeaturesCard.jsx
@@ -1,11 +1,17 @@
 import './KeyFeaturesCard.css'
 
-export function KeyFeaturesCard({heading, para}) {
+
+export function KeyFeaturesCard({heading, para, pic}) {
     return (
-        <section className="main">
-            <h1 className="main-heading">{heading}</h1>
-            <hr className="card-hr"/>
-            <p className="main-para">{para}</p>
+        <section className="key-features-card-main">
+            <div className="key-features-card-main-div">
+                <img className="key-features-card-main-div-img" src={pic} alt="Data Extraction"/>
+            </div>
+            <div className="key-features-card-main-div-two">
+                <h1 className="key-features-card-main-h1">{heading}</h1>
+                <p className="key-features-card-main-p">{para}</p>
+            </div>
+
         </section>
     )
 }
