11c3a36cac197f956fe8db634fde4bc6618d4314
Ujjwal-here <ujjwalsbx@gmail.com>
2023-10-09 15:31:19 +0530

    Rename ClassNames

--- src/reusable/HowExtensionWorksCard.jsx
+++ src/reusable/HowExtensionWorksCard.jsx
@@ -1,20 +1,25 @@
 import {HiCheckCircle} from "react-icons/hi2"
 import "./HowExtensionWorksCard.css"
 
-export function HowExtensionWorksCard({heading, paraOne, paraTwo}) {
+export function HowExtensionWorksCard({heading, paraOne, paraTwo, pic}) {
     return (
         <>
-            <section className="main">
-                <h1 className="main-heading">{heading}</h1>
-                <hr className="card-hr"/>
-                <div className="main-div">
-                    <HiCheckCircle className="icons"/>
-                    <span className="main-para">{paraOne}</span>
+            <section className="extension-card-main">
+                <div className="extension-card-main-div">
+                    <img className="extension-card-main-div-img" src={pic} alt="Meeting"/>
                 </div>
-                <div className="main-div">
-                    <HiCheckCircle className="icons"/>
-                    <span className="main-para">{paraTwo}</span>
+                <div className="extension-card-main-div-two">
+                    <h1 className="extension-card-main-div-two-h1">{heading}</h1>
+                    <div className="extension-card-main-div-two-div">
+                        <HiCheckCircle className="extension-card-main-div-two-div-icons"/>
+                        <span className="extension-card-main-div-two-div-p">{paraOne}</span>
+                    </div>
+                    <div className="extension-card-main-div-two-div">
+                        <HiCheckCircle className="extension-card-main-div-two-div-icons"/>
+                        <span className="extension-card-main-div-two-div-p">{paraTwo}</span>
+                    </div>
                 </div>
+
             </section>
         </>
     )
