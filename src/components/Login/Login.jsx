6eef889c3befa3de9a5e839bc7319250a0363a6c
Ujjwal-here <ujjwalsbx@gmail.com>
2023-10-10 15:57:20 +0530

    Created Login Page

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,9 @@
+import {Auth} from "../Auth";
+import LoginImage from "../../assets/login.svg"
+
+export function Login() {
+    return (
+        <Auth btn_name="Login" image={LoginImage} p="Please Login into your Meet-Brief Account" link_to="/signup"
+              option_title="Don't have an Acount?" option_btn="Signup"/>
+    )
+}
